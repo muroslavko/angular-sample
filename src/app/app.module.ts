bd3c7cf1dc43039c9f143a2a3db852c48987fdc6
Myroslav Dmytrus <myroslavdm@gmail.com>
2019-07-03 21:47:32 +0300

    Created simple components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app-root/app.component';
+import { BookListComponent } from './book/book-list/book-list.component';
+import { BookDetailsComponent } from './book/book-details/book-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent,
+    BookDetailsComponent
   ],
   imports: [
     BrowserModule,
