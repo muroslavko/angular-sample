dea888f10178b6b0dc4180a39c26f096f606943b
Myroslav Dmytrus <myroslavdm@gmail.com>
2019-07-06 19:20:35 +0300

    Created title two-way data binding

--- src/app/book/book-details/book-details.component.ts
+++ src/app/book/book-details/book-details.component.ts
@@ -1,18 +1,26 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Book } from 'src/app/models/book';
 
 @Component({
-	selector: 'book-details',
-	templateUrl: './book-details.component.html',
-	styleUrls: ['./book-details.component.css']
+    selector: 'book-details',
+    templateUrl: './book-details.component.html',
+    styleUrls: ['./book-details.component.css']
 })
 export class BookDetailsComponent implements OnInit {
 
-	@Input() item: Book;
-	@Input() titleColor: string;
+    @Input() item: Book;
+    @Input('bookTitle') title: string;
+    @Output('bookTitleChange') titleChange = new EventEmitter<string>();
+    @Input() titleColor: string;
 
-	constructor() { }
+    editTitleHidden = true;
 
-	ngOnInit() {
-	}
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+    titleChanged() {
+        this.titleChange.emit(this.title);
+    }
 }
