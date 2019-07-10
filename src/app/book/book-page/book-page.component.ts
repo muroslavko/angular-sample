ab319681c2b4af15a4c59b1d6d0f545103d62818
Myroslav Dmytrus <myroslavdm@gmail.com>
2019-07-10 20:46:28 +0300

    Created service

--- src/app/book/book-page/book-page.component.ts
+++ src/app/book/book-page/book-page.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from 'src/app/models/book';
+import { BookService } from 'src/app/services/book.service';
 
 @Component({
 	selector: 'app-book-page',
 	templateUrl: './book-page.component.html',
-	styleUrls: ['./book-page.component.css']
+    styleUrls: ['./book-page.component.css'],
+    providers: [BookService]
 })
 export class BookPageComponent implements OnInit {
 
@@ -12,20 +14,10 @@ export class BookPageComponent implements OnInit {
     books: Book[];
     selectedBook: Book = {} as Book;
     selectedBookIndex: number = -1;
-	constructor() { }
+	constructor(private bookService: BookService) { }
 
 	ngOnInit() {
-        this.books = [{
-            author: 'Stephen Edwin King',
-            description: 'Jake Epping is a recently divorced high school English teacher in Lisbon Falls, Maine, earning extra money teaching a GED class.',
-            publishedDate: new Date(2011, 11, 8),
-            title: '11/22/63'
-        },{
-            author: 'J. R. R. Tolkien',
-            description: 'The Hobbit is set within Tolkien`s fictional universe and follows the quest of home-loving hobbit Bilbo Baggins to win a share of the treasure guarded by Smaug the dragon.',
-            publishedDate: new Date(1937, 9, 21),
-            title: 'The Hobbit, or There and Back Again'
-        }]
+        this.books = this.bookService.getBooks();
     }
     
     showEdit() {
---
+++ src/app/services/book.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Book } from '../models/book';
+
+@Injectable(
+//     {
+//     providedIn: 'root'
+// }
+)
+
+export class BookService {
+
+    constructor() { }
+
+
+    getBooks(): Book[] {
+        return [{
+            author: 'Stephen Edwin King',
+            description: 'Jake Epping is a recently divorced high school English teacher in Lisbon Falls, Maine, earning extra money teaching a GED class.',
+            publishedDate: new Date(2011, 11, 8),
+            title: '11/22/63'
+        }, {
+            author: 'J. R. R. Tolkien',
+            description: 'The Hobbit is set within Tolkien`s fictional universe and follows the quest of home-loving hobbit Bilbo Baggins to win a share of the treasure guarded by Smaug the dragon.',
+            publishedDate: new Date(1937, 9, 21),
+            title: 'The Hobbit, or There and Back Again'
+        }];
+    }
+}
